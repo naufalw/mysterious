603c9506463fa499522c1afd0950a31ad8832a29
Faris Rafie Syahzani <farisyahzani6@gmail.com>
2025-02-16 09:09:42 +1100

    try yt search but mambu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Union, List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from youtube_transcript_api import YouTubeTranscriptApi
+from python_yt_search import VideosSearch, PlaylistsSearch, Search
 
 app = FastAPI()
 
@@ -12,7 +13,7 @@ def read_root():
 
 @app.get("/search/{query}")
 def search_query(query: str):
-    return [x for x in search(query, advanced=True)]
+    return [x for x in search(query, advanced=True, num_results=20)]
 
 class SearchQuery(BaseModel):
     query: str
@@ -24,6 +25,18 @@ class SearchQueries(BaseModel):
 @app.get("/youtube/{video_id}")
 def getTranscript(video_id: str):
     return " ".join(x["text"] for x in YouTubeTranscriptApi.get_transcript(video_id))
+
+# @app.get("/search/{query}")
+# async def getTranscript(query: str):
+#     videos = VideosSearch(query, limit = 3)
+#     playlist = PlaylistsSearch(query, limit = 2)
+
+#     res_videos = [{"url":v["link"], "title": v["title"], "description": " ".join([x["text"] for x in v["descriptionSnippet"]])} for v in videos.result()["result"]]
+#     res_playlist = [{"url":v["link"], "title": v["title"], "description": " ".join([x["text"] for x in v["descriptionSnippet"]])} for v in playlist.result()["result"]]
+
+#     allSearch = Search(query, limit = 10)
+
+#     return allSearch.result()
 
 
 
