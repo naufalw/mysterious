35b7577e47939fc8b543387b3546da05517b170e
naufalw <naufalwiwitputra@outlook.com>
2025-02-22 10:16:47 +1100

    mod: bypass cors

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from youtube_transcript_api import YouTubeTranscriptApi
 from python_yt_search import VideosSearch, PlaylistsSearch, Search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    '*'
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
@@ -25,28 +40,3 @@ class SearchQueries(BaseModel):
 @app.get("/youtube/{video_id}")
 def getTranscript(video_id: str):
     return " ".join(x["text"] for x in YouTubeTranscriptApi.get_transcript(video_id))
-
-# @app.get("/search/{query}")
-# async def getTranscript(query: str):
-#     videos = VideosSearch(query, limit = 3)
-#     playlist = PlaylistsSearch(query, limit = 2)
-
-#     res_videos = [{"url":v["link"], "title": v["title"], "description": " ".join([x["text"] for x in v["descriptionSnippet"]])} for v in videos.result()["result"]]
-#     res_playlist = [{"url":v["link"], "title": v["title"], "description": " ".join([x["text"] for x in v["descriptionSnippet"]])} for v in playlist.result()["result"]]
-
-#     allSearch = Search(query, limit = 10)
-
-#     return allSearch.result()
-
-
-
-# @app.post("/searchbatch")
-# def search_batch(queries: SearchQueries):
-#     results = []
-#     for query in queries.data:
-#         results.append({
-#             "nodeId": query.nodeId,
-#             "results": [x for x in search(query.query)]
-#         })
-#     return results
-
